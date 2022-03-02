0af06f1dabf903dba7e5ed68100ca54ab9d05610
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-02 14:30:50 -0800

    added people array to data.js

---
+++ data.js
@@ -0,0 +1,16 @@
+export const people = [
+    {
+        id: 1,
+        name: 'Cody',
+        pronouns: 'he/him',
+        favorite_place: 'Multnomah Falls',
+        hobbies: ['video games', 'cooking', 'hanging out with friends']
+    },
+    {
+        id: 2,
+        name: 'Riley',
+        pronouns: 'he/him',
+        favorite_place: 'Mt Bachelor',
+        hobbies: ['skiing', 'watching sports', 'hanging out with friends']
+    }
+];
