b0c0d9a3fdf39c93cbc25e737be4fdc71d88879e
Cody-Wise <cody@sharptackmedia.com>
2022-03-02 14:50:02 -0800

    Grabbed Dom Elements

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ console.log(people, 'people');
 
 // grab DOM elements
 
+const selectEl = document.querySelector('.select');
+
+const divEl = document.querySelector('.contain');
+
+const nameEl = document.querySelector('.name');
+
+const pronounsEl = document.querySelector('.pronouns');
+
+const favPlaceEl = document.querySelector('.favorite_place');
+
+const hobbiesEl = document.querySelector('.hobbies');
+
+console.log(favPlaceEl);
+
 // set event listeners 
     // get user input
     // use user input to update state 
