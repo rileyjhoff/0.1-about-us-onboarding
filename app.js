76d1394c82da8e120bcde0520e9f72384440915e
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-02 16:17:51 -0800

    adjusted some html

--- app.js
+++ app.js
@@ -9,17 +9,11 @@ import { people } from './data.js';
 // grab DOM elements
 
 const selectEl = document.querySelector('.select');
-
 const divEl = document.querySelector('.contain');
-
 const nameEl = document.querySelector('.name');
-
 const pronounsEl = document.querySelector('.pronouns');
-
 const favPlaceEl = document.querySelector('.favorite_place');
-
 const hobbiesEl = document.querySelector('.hobbies');
-
 const hobbiesDisplayEl = document.querySelector('.hobbies_display');
 
 // console.log(favPlaceEl);
